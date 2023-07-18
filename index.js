003b15f54e0970788fa50527f6553275fd4abdad
root <+>
2023-07-17 18:59:07 -0600

    a

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+main.remove();
+
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+
+let newHeader = document.getElementById('victory'); // select the h1 element with id 'victory'
+newHeader.innerText = 'Baran is the champion'; // change its text
+
